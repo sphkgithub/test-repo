65a02f535087bb1c6317ab22f5b5f713f0c92953
sphkgithub <software.professionalhk@gmail.com>
2020-12-26 18:19:30 +0800

    updated

---
+++ POcApp/src/main/java/com/poc/Greeting.java
@@ -0,0 +1,18 @@
+package com.poc;
+
+public class Greeting {
+    public Greeting() {
+        super();
+    }
+    
+    public void printDummy()
+    {
+        System.out.println("JAVA POC---> DUMMY");
+    }
+
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        greeting.printDummy();
+        System.out.println("JAVA POC---> HELLO WORLD");
+    }
+}
